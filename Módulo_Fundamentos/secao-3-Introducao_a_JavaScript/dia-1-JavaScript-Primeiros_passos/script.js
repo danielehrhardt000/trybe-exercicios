5a29cc047aaa585bdb70172153fa7a2c0904ccb9
Daniel da Cruz Ehrhardt <daniel.cruz.ehrhardt25+github@gmail.com>
2023-01-24 11:57:32 -0300

    Exercícios de if/else feito.

--- Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-1-JavaScript-Primeiros_passos/script.js
+++ Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-1-JavaScript-Primeiros_passos/script.js
@@ -1,18 +1,56 @@
-let a = 20
-let b = 35
+// If/Else
 
-let adicao = a + b;
-console.log(adicao);
+// let a = 20
+// let b = 35
 
-let sub = a - b;
-console.log(sub);
+// let adicao = a + b;
+// console.log(adicao);
 
-let multi = a*b;
-console.log(multi);
+// let sub = a - b;
+// console.log(sub);
 
-let divi = a/b;
-console.log(divi);
+// let multi = a*b;
+// console.log(multi);
 
-let mol = a%b;
-console.log(mol);
+// let divi = a/b;
+// console.log(divi);
+
+// let mol = a%b;
+// console.log(mol);
+
+// let a = 45
+// let b = 45
+// let c = 90
+
+// if (a > b){
+//     console.log('a é maior');
+// }else {
+//     console.log('b é maior');
+// }
+
+// if (a > b && a > c){
+//     console.log('a é maior');
+// } else if (b > a && b > c) {
+//     console.log('b é maior');
+// } else if (c > a && c > a) {
+//     console.log('c é maior');
+// }
+
+// if (a > 0) {
+//     console.log('positive');
+// } else if (a < 0) {
+//     console.log('negative');
+// } else {
+//     console.log('zero');
+// }
+
+// if (a === 45 && b === 45 && c === 90) {
+//     console.log(true);
+// } else if (a < 45 || b < 45 || c < 90) {
+//     console.log(false);
+// } else if (a <= 0 || b <= 0 || c <= 0) {
+//     console.log('valor inválido');
+// }
+
+// Switch/Case
 
