749f5adc119afd3603608988778c85676e46cfae
Daniel da Cruz Ehrhardt <daniel.cruz.ehrhardt25+github@gmail.com>
2023-01-24 11:32:21 -0300

    Primeiro dia JavaScript

---
+++ Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-1-JavaScript-Primeiros_passos/script.js
@@ -0,0 +1,18 @@
+let a = 20
+let b = 35
+
+let adicao = a + b;
+console.log(adicao);
+
+let sub = a - b;
+console.log(sub);
+
+let multi = a*b;
+console.log(multi);
+
+let divi = a/b;
+console.log(divi);
+
+let mol = a%b;
+console.log(mol);
+
