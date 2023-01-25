7a14375a16921751c0e8872ddb100b45224b5693
Daniel da Cruz Ehrhardt <daniel.cruz.ehrhardt25+github@gmail.com>
2023-01-25 14:09:41 -0300

    Exercício parte 8

--- Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-2-JavaScript-Array_e_loop_For/script.js
+++ Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-2-JavaScript-Array_e_loop_For/script.js
@@ -1,19 +1,23 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+// Parte 1
 // console.log(numbers.sort());
 
+// Parte 2
 // let sum = 0
 // for (let index = 0; index < numbers.length; index+=1) {
 //     sum+=numbers[index]
 // }
 // console.log(sum);
 
+// Parte 3
 // let sum = 0
 // for (let index = 0; index < numbers.length; index+=1) {
 //     sum+=numbers[index]/10
 // }
 // console.log(sum);
 
+// Parte 4
 // let sum = 0
 // for (let index = 0; index < numbers.length; index+=1) {
 //     sum+=numbers[index]/10
@@ -25,16 +29,24 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 // console.log(sum);
 
-function largestNumbers(numbers){
-    let largestNumber = [0];
-    for (let index = 0; index < numbers.length; index+=1) {
-        for (let larIndex = 0; larIndex < numbers[index].length; larIndex+=1) {
-            if (numbers[index][larIndex] > largestNumber[index]) {
-                largestNumber[index] = numbers[index][larIndex];
-            }
-    }
-    
-}
-return largestNumber;
-}
-// console.log(largestNumber);
+// Parte 5
+// // for (let index = 0; index < numbers.length; index+=1) {
+//     console.log(Math.max.apply(null, numbers));    
+// }
+
+// Parte 6
+// let impar = 0
+// for (let index = 0; index < numbers.length; index+=1) {
+//     if (numbers[index]%2 !== 0)
+//     {
+//         impar++;
+//     }
+// }
+// console.log('Os números ímpares são: ' + impar);
+
+// Parte 7
+// for (let index = 0; index < numbers.length; index+=1) {
+//     console.log(Math.min.apply(null, numbers));    
+// }
+
+// Parte 8
