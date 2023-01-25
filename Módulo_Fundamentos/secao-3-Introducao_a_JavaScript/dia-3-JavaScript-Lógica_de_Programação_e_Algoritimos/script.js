946085196e8a84097b108d95cd02ebac2bd87a91
Daniel da Cruz Ehrhardt <daniel.cruz.ehrhardt25+github@gmail.com>
2023-01-25 20:55:04 -0300

    Exercícios dia 3 parte 2

--- Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-3-JavaScript-Lógica_de_Programação_e_Algoritimos/script.js
+++ Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-3-JavaScript-Lógica_de_Programação_e_Algoritimos/script.js
@@ -1,8 +1,25 @@
-let fatorial = num => {
-    if (num > 1) {
-        return num * fatorial(num - 1)
-    }
-    return num
-}
+// Exercícios dia 3
 
-console.log(fatorial(10));
+// Parte 1 - fatorial
+// let fatorial = num => {
+//     if (num > 1) {
+//         return num * fatorial(num - 1)
+//     }
+//     return num
+// }
+
+// console.log(fatorial(10));
+
+// Parte 2 - Inverter palavra
+// let word = 'tryber'
+// for (let index = 0; index < word.length; index+=1) {
+//     word.push(word[index].split('').reverse().join(''));
+// }
+function reverse(s) {
+    let word = 'tryber';
+    for (let index = s.length - 1, join = 0; index >= 0; index -=1) {
+        word += s[index];        
+    }
+    return word;
+}
+console.log(reverse(s));
