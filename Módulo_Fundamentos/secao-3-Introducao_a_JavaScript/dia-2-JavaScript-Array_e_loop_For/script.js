036ab825f07595d8536d6e004f7f2235f20a9345
Daniel da Cruz Ehrhardt <daniel.cruz.ehrhardt25+github@gmail.com>
2023-01-25 11:24:29 -0300

    Exercícios JavaScript dia 2

---
+++ Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-2-JavaScript-Array_e_loop_For/script.js
@@ -0,0 +1,22 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// console.log(numbers.sort());
+
+// let sum = 0
+// for (let index = 0; index < numbers.length; index+=1) {
+//     sum+=numbers[index]
+// }
+// console.log(sum);
+
+// let sum = 0
+// for (let index = 0; index < numbers.length; index+=1) {
+//     sum+=numbers[index]/10
+// }
+// console.log(sum);
+
+let sum = 0
+for (let index = 0; index < numbers.length; index+=1) {
+    sum+=numbers[index]/10
+}
+
+console.log(sum);
