928d62f88bb0859a30fef48d25e02a91d758edaa
Daniel da Cruz Ehrhardt <daniel.cruz.ehrhardt25+github@gmail.com>
2023-01-25 19:08:04 -0300

    Parte 1 dos exercícios do dia 3 feito

--- Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-3-JavaScript-Lógica_de_Programação_e_Algoritimos/script.js
+++ Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-3-JavaScript-Lógica_de_Programação_e_Algoritimos/script.js
@@ -1,8 +1,8 @@
-let num = 0;
-let mensagem = 'A soma total de 1 a 50 é: '
+let fatorial = num => {
+    if (num > 1) {
+        return num * fatorial(num - 1)
+    }
+    return num
+}
 
- for (let index = 1; index <= 50; index+=1) {
-    num += index;
- }
- mensagem += num
- console.log(mensagem);
+console.log(fatorial(10));
