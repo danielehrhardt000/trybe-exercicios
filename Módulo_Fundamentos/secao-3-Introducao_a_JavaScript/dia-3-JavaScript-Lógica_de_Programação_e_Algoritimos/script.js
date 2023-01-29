e6917046db4a7cc911225489258c30c2fd95437b
Daniel da Cruz Ehrhardt <daniel.cruz.ehrhardt25+github@gmail.com>
2023-01-29 19:50:43 -0300

    Exercícios dia 3 de JavaScript concluído.

--- Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-3-JavaScript-Lógica_de_Programação_e_Algoritimos/script.js
+++ Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-3-JavaScript-Lógica_de_Programação_e_Algoritimos/script.js
@@ -7,6 +7,14 @@
 //     }
 //     return num
 // }
+// console.log(fatorial(10));
+// // ou
+// let fatorials = 1;
+
+// for (let index = 10; index > 0; index-=1) {
+//     fatorials *= index;
+// }
+// console.log(fatorials);
 
 // console.log(fatorial(10));
 
@@ -20,12 +28,36 @@
 // console.log(reverseWord);
 
 // Parte - 3 - Maior e menor palavra
-let palavras = 'oi, meu, chapa, tudo, bem, com, você';
-let maior = ''
-let menor = ''
+// let palavras = ['oi, meu, chapa, tudo, bem, com, você'];
+// let maior = palavras[0];
+// let menor = palavras[0];
 
-for (let index = 0; index < palavras.length; index+=1) {
-    match(/\w+/g)
-    maior += palavras[index]
-}
-console.log(maior);
+// for (let index = 0; index < palavras.length; index+=1) {
+//     if (palavras[index].length > maior.length) {
+//         maior = palavras[index];
+//     }
+// }
+
+// for (let index = 0; index < palavras.length; index+=1) {
+//     if (palavras[index].length < menor.length) {
+//         menor = palavras[index];
+//     }
+// }
+// console.log(maior);
+// console.log(menor);
+
+// Parte - 4 - Maior número primo
+// let numeroPrimo = 0;
+
+// for (let indexNumero = 2; indexNumero <= 50; indexNumero += 1) {
+//     let ePrimo = true;
+//     for (let indexDivisor = 2; indexDivisor < indexNumero; indexDivisor += 1) {
+//         if (indexNumero % indexDivisor === 0) {
+//             ePrimo = false;
+//         }
+//     }
+//     if (ePrimo) {
+//         numeroPrimo = indexNumero;
+//     }
+// }
+// console.log(numeroPrimo);
