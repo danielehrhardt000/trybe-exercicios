9acc6ea322628b88323600689527009a13863ead
Daniel da Cruz Ehrhardt <daniel.cruz.ehrhardt25+github@gmail.com>
2023-01-29 18:54:34 -0300

    Exercício bônus dia 2 concluído.

--- Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-2-JavaScript-Array_e_loop_For/bonus.js
+++ Módulo_Fundamentos/secao-3-Introducao_a_JavaScript/dia-2-JavaScript-Array_e_loop_For/bonus.js
@@ -1,14 +1,42 @@
-let numbers =  [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let ordenado;
+// Parte 1 - Ordenando em ordem crescente
+// let numbers =  [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let ordenado;
 
-for (let index = 1; index < numbers.length; index+=1) {
-    for (let index2 = 0; index2 < index; index2+=1) {
-        if (numbers[index] < numbers[index2]) {
-            let position = numbers[index];
-            numbers[index] = numbers[index2];
-            numbers[index2] = position;
-        }
-        return ordenado
-    }
-}
-console.log(ordenado);
+// for (let index = 1; index < numbers.length; index+=1) {
+//     for (let index2 = 0; index2 < index; index2+=1) {
+//         if (numbers[index] < numbers[index2]) {
+//             let position = numbers[index];
+//             numbers[index] = numbers[index2];
+//             numbers[index2] = position;
+//         }
+//     }
+// }
+// console.log(numbers);
+
+// Parte 2 - Ordenando em ordem decrescente
+// let numbers =  [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let ordenado;
+
+// for (let index = 1; index < numbers.length; index+=1) {
+//         for (let index2 = 0; index2 < index; index2+=1) {
+//             if (numbers[index] > numbers[index2]) {
+//                 let position = numbers[index];
+//                 numbers[index] = numbers[index2];
+//                 numbers[index2] = position;
+//             }
+//         }
+// }
+// console.log(numbers);
+
+// Parte 3 - Multiplicando o seguinte
+// let numbers =  [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let newArray = [];
+
+// for (let index = 0; index < numbers.length; index+=1) {
+//     if (index + 1 < numbers.length) {
+//         newArray.push(numbers[index] * numbers[index + 1]);
+//     } else {
+//         newArray.push(numbers[index] * 2);
+//     }
+// }
+// console.log(newArray);
