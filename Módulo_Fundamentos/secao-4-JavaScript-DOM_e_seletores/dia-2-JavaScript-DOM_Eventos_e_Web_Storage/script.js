18d7b05163aebab1c418b99828fdfbc97f032fe3
Daniel da Cruz Ehrhardt <daniel.cruz.ehrhardt25+github@gmail.com>
2023-02-01 15:18:06 -0300

    Exercício JavaScript s4: Utilizando asquivos teste para buscar elementos.

---
+++ Módulo_Fundamentos/secao-4-JavaScript-DOM_e_seletores/dia-2-JavaScript-DOM_Eventos_e_Web_Storage/script.js
@@ -0,0 +1,13 @@
+document.getElementById('elementoOndeVoceEsta').parentNode.style.backgroundColor = 'rgb(74, 178, 195)';
+
+document.getElementById('primeiroFilhoDoFilho').innerText = 'Estou usando o JavaScript para editar esse HTML.';
+
+document.getElementById('pai').firstElementChild;
+
+document.getElementById('elementoOndeVoceEsta').previousElementSibling;
+
+document.getElementById('elementoOndeVoceEsta').nextSibling;
+
+document.getElementById('elementoOndeVoceEsta').nextElementSibling;
+
+document.getElementById('pai').firstElementChild.nextElementSibling.nextElementSibling;
